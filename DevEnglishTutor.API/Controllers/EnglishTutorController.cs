0d28878435b707aef83bc3a81fa97d3c30d1f33c
NATANAEL <NATANAEL@DESKTOP-RDK7QEU>
2023-02-22 16:41:53 -0300

    Adicionar arquivos de projeto.

---
+++ DevEnglishTutor.API/Controllers/EnglishTutorController.cs
@@ -0,0 +1,34 @@
+﻿using DevEnglishTutor.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace DevEnglishTutor.API.Controllers {
+    [ApiController]
+    [Route("api/english-tutor")]
+    public class EnglishTutorController : ControllerBase {
+
+        private readonly HttpClient _httpClient;
+        public EnglishTutorController(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration) {
+
+            var token = configuration.GetValue<string>("MyUserSecrets");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var model = new ChatGptInputModel(text);
+            var requestBody = JsonSerializer.Serialize(model);
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+            var result = await response.Content.ReadFromJsonAsync<ChatGptResponseModel>();
+            var promptResponse = result.choices.FirstOrDefault();
+            return Ok(promptResponse.text.Replace("\n", "").Replace("\t", ""));
+
+        }
+    }
+}
---
+++ DevEnglishTutor.API/Models/ChatGptInputModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DevEnglishTutor.API.Models {
+    public class ChatGptInputModel {
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
+        public ChatGptInputModel(string prompt) {
+            this.prompt = $"Correct this english phrase: {prompt}";
+            temperature = 0.2m;
+            max_tokens = 150;
+            model = "text-davinci-003";
+        }
+    }
+}
